f75f7e1ba9b746018db98d7cd81ae2ca9939a46e
TylerFettig32 <70714922+TylerFettig32@users.noreply.github.com>
2020-09-23 12:14:13 -0700

    Create output 1-10 with one variable.cpp

---
+++ output 1-10 with one variable.cpp
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int t=1, y=2, l=3, e=4, r=5, g=6, k=7, m=8, a=9, q=10;
+    
+    
+    
+    printf("Hello World \n");
+    printf("t=%d \n",t);
+    printf("y=%d \n",y);
+    printf("l=%d \n",l);
+    printf("e=%d \n",e);
+    printf("r=%d \n",r);
+    printf("g=%d \n",g);
+    printf("k=%d \n",k);
+    printf("m=%d \n",m);
+    printf("a=%d \n",a);
+    printf("q=%d \n",q);
+
+    return 0;
+}
